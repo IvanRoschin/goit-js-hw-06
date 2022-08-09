ef1b62a905b41c269b9ef54ec0e23007caf5dd87
Ivan Roschin <ivan.roschin86@gmail.com>
2022-08-09 18:43:05 +0300

    update 03

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,8 @@
+/**
+ * Используй массив объектов images для создания элементов <img> вложенных в <li>. Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+ * Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+ * Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
+ */
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +17,27 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galleryCountainerEl = document.querySelector('.gallery');
+galleryCountainerEl.classList.add('grid');
+
+
+const itemEl = document.createElement('li');
+itemEl.classList.add('item');
+
+const image = document.createElement('img');
+image.classList.add('image');
+// image.src = imageLink;
+// image.alt = imageAlt;
+
+itemEl.append(image);
+
+// itemEl.insertAdjacentElement('afterbegin', 'gallery');
+
+console.log(itemEl);
+
+// const elements = images.map(({ imageLink, imageAlt }) => { 
+
+// })
+
+// itemEl.insertAdjacentElement('afterbegin', 'image');
