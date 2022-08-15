4bcbd6df124c9ca1686d5e7de8cadd41fb6fbb0c
Ivan Roschin <ivan.roschin86@gmail.com>
2022-08-15 16:29:18 +0300

    update tasak-10

--- js/task-10.js
+++ js/task-10.js
@@ -13,67 +13,51 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const controlsEl = document.querySelector('#controls');
 
-const numberEl = document.querySelector('input');
-console.log(numberEl);
+const numberEl = controlsEl.firstElementChild;
+// console.log(numberEl);
 
 const createBtnEl = document.querySelector('[data-create]');
-console.log(createBtnEl);
+// console.log(createBtnEl);
 
 const destroyBtnEl = document.querySelector('[data-destroy]');
-console.log(destroyBtnEl);
+// console.log(destroyBtnEl);
 
 const boxesEl = document.querySelector('#boxes');
-console.log(boxesEl);
+// console.log(boxesEl);
 
 
 numberEl.addEventListener('input', onInputClick);
 
+//! Получаем значение количества div
+function onInputClick(event) {
 
-function onInputClick(event) { 
-
-  let amount = Number(event.currentTarget.value); 
-  console.log(amount);
+  let amount = event.currentTarget.value;
+  console.log('amount', amount);
 
   createBtnEl.addEventListener('click', createBoxes);
   destroyBtnEl.addEventListener('click', onDestroyBtnElClick);
 
-//! Цикл создания дива
-  function createBoxes(event) { 
-       boxesEl.insertAdjacentHTML('afterbegin', '<div><div/>');
-
-  //   console.log(amount);
-    
-  //   for (let i = 0; i < amount; i -= 1) {
-  //     console.log(i[0]);
-  // if (amount > 0) {
-  //  boxesEl.insertAdjacentHTML('afterbegin', '<div><div/>');
-  //   continue;
-  // }
-
-  console.log(boxesEl); 
-// }
-    // if (amount > 0) {
-      // console.log(amount);
-      // const divEl = document.createElement('div');
-      // divEl.style.backgroundColor = getRandomHexColor();
-      // divEl.style.width = '30px';
-      // divEl.style.height = '30px';
-      // divEl.appendChild(boxesEl);
-      // boxesEl.insertBefore(divEl, boxesEl.firstElementChild);
-    // };
-
-
-
-    
-    // boxesEl.insertAdjacentHTML('afterbegin', '<div><div/>');
+//! Цикл создания div
+  function createBoxes() { 
+  for (let i = 1; i <= amount; i += 1) {
+    let widthAndHeigthBase = 30;
+    const divEl = document.createElement('div');
+    divEl.style.backgroundColor = getRandomHexColor();
+    divEl.style.width = `${widthAndHeigthBase}` + `px`;
+    divEl.style.height = `${widthAndHeigthBase}`+ `px`;
+    boxesEl.append(divEl);
+    console.log(boxesEl);
+    console.log(widthAndHeigthBase);
+    };
   };
-
+    
+//! Очищаем div#boxes
 function onDestroyBtnElClick() { 
   boxesEl.innerHTML = '';
-  console.log('div#boxes очищен!')
+  numberEl.value = '';
+  amount = '';
+  console.log('div#boxes очищен!', amount)
   };
-  
-
-};
-
+ };
