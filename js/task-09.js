4edcb9ac6d9af3421a29e67a5de62013b4fb3987
Ivan Roschin <ivan.roschin86@gmail.com>
2022-08-17 05:59:30 +0300

    update-09
    Исправляет:
    1) изменение цвета на body;
    2) информация о цвете выведена в span

--- js/task-09.js
+++ js/task-09.js
@@ -2,22 +2,21 @@
  *!Задание № 9
  * Напиши скрипт, который изменяет цвета фона элемента
  *  <body> через инлайн стиль при клике на
- *  button.change-color и выводит значение цвета в span.color. 
+ *  button.change-color и выводит значение цвета в span.color.
  */
 
-const changeColorBtn = document.querySelector('.change-color');
-const changeColorEl = document.querySelector('.color');
-changeColorBtn.addEventListener('click', onClickToChangeColorBtn);
+const changeColorBtn = document.querySelector(".change-color");
+const ColorInfoEl = document.querySelector(".color");
+const changeColorEl = document.querySelector("body");
+
+changeColorBtn.addEventListener("click", onClickToChangeColorBtn);
 
 function getRandomHexColor() {
-      return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  };
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
-function onClickToChangeColorBtn(event) { 
+function onClickToChangeColorBtn() {
   changeColorEl.style.backgroundColor = `${getRandomHexColor()}`;
-};
+  ColorInfoEl.textContent = `${getRandomHexColor()}`;
+}
 console.log(changeColorEl);
-
-
-
-
--- js/task-10.js
+++ js/task-10.js
@@ -13,53 +13,43 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-
 const refs = {
-
-  inputEl: document.querySelector('#controls > input'),
-  createBtnEl: document.querySelector('[data-create]'),
-  destroyBtnEl: document.querySelector('[data-destroy]'),
-  boxesCountainerEl: document.querySelector('#boxes'),
-}
+  inputEl: document.querySelector("#controls > input"),
+  createBtnEl: document.querySelector("[data-create]"),
+  destroyBtnEl: document.querySelector("[data-destroy]"),
+  boxesCountainerEl: document.querySelector("#boxes"),
+};
 
 const baseSizes = 20;
 
-const divMarkcup = () => { 
-  const divEl = document.createElement('div');
-  divEl.style.backgroundColor = getRandomHexColor();
-  divEl.style.width = `${baseSizes} + "px"`;
-  console.log(divEl.style.width);
-  divEl.style.height = `${baseSizes} + "px"`;
-  refs.boxesCountainerEl.append(divEl);
-  console.log(refs.boxesCountainerEl);
-
-  return divEl;
-}
- 
-
-refs.inputEl.addEventListener('input', onInputClick);
+refs.inputEl.addEventListener("input", onInputClick);
 
 //! Получаем значение amount
 function onInputClick(event) {
-
   let amount = event.currentTarget.value;
-  console.log('amount', amount);
+  console.log("amount", amount);
 
-  refs.createBtnEl.addEventListener('click', createBoxes);
-  refs.destroyBtnEl.addEventListener('click', onDestroyBtnElClick);
+  refs.createBtnEl.addEventListener("click", createBoxes);
+  refs.destroyBtnEl.addEventListener("click", onDestroyBtnElClick);
 
-//! Цикл создания div
-  function createBoxes() { 
-  for (let i = 1; i <= amount; i += 1) {
-    divMarkcup;
-    };
-  };
-    
-//! Очищаем div#boxes
-function onDestroyBtnElClick() { 
-  refs.boxesCountainerEl.innerHTML = '';
-  refs.inputEl.value = '';
-  amount = '';
-  console.log('div#boxes очищен!', amount)
-  };
- };
+  //! Цикл создания div
+  function createBoxes() {
+    for (let i = 1; i <= amount; i += 1) {
+      const divEl = document.createElement("div");
+      divEl.style.backgroundColor = getRandomHexColor();
+      divEl.style.width = `${baseSizes + 10 + "px"}`;
+      console.log(divEl.style.width);
+      divEl.style.height = `${baseSizes + 10 + "px"} `;
+      refs.boxesCountainerEl.append(divEl);
+      console.log(refs.boxesCountainerEl);
+    }
+  }
+
+  //! Очищаем div#boxes
+  function onDestroyBtnElClick() {
+    refs.boxesCountainerEl.innerHTML = "";
+    refs.inputEl.value = "";
+    amount = "";
+    console.log("div#boxes очищен!", amount);
+  }
+}
